abdfc73b8ea6d1c3536fa2a1838b1cdc98e7d692
Clover <100253140+Cloveriow@users.noreply.github.com>
2022-12-20 00:35:16 +0800

    Added output txt function
    Added output txt function.
    Delete function under development...

--- spell_text.py
+++ spell_text.py
@@ -1,4 +1,4 @@
-import plac
+import os,plac
 import re
 
 Promptlist=[]
@@ -11,7 +11,7 @@ def main():
     global Prompt
     global Weights
     power = 0
-    print("Enter the prompt and weight Format:prompt,weight example:\n1girl,2.0\n1girl:2.0\nEnter -i or info get more information\nYou can enter continuously or with a single input\nEnter 0 to leave the loop and display the result.\n")
+    print("Enter the prompt and weight Format:prompt,weight example:\n1girl,2.0\n1girl:2.0\nEnter -i or info get more information.\nEnter -o or output to generate txt from the input data.\nYou can enter continuously or with a single input.\nEnter 0 to leave the loop and display the result.\n")
     while (power ==0):
         request = input()
         if (request=="0"):
@@ -24,12 +24,21 @@ def main():
 
 
 def process(request):
+    global x
+    global Prompt
+    global Weights
     if (request.lower() == "info")|(request.lower() =="-i"):
         print("The program will automatically delete the input {}() and newline\nfollowing is an example\n{masterpiece},2.0 -->(masterpiece:2.0)\nmasterpiece,1.5,best quality,1.3,Highest image quality,1.2 -->(masterpiece:1.5),(best quality:1.3),(Highest image quality:1.2),\nI'm Cloveriow, I want to make a sorting method that automatically sorts Tags,\nbut the operation screen of Python is not ideal, and I will develop in C# in the future.")
+    elif (request.lower() == "output")|(request.lower() =="-o"):
+        path = 'output.txt'
+        print("output path:\n"+os.path.dirname(os.path.abspath(__file__))+"output.txt")
+        f = open(path, 'w')
+        for i in range(len(Prompt)):
+            print("("+Prompt[i]+":"+Weights[i]+"),",end = '',file=f)
+        f.close()
+    elif (request.lower() == "delete")|(request.lower() =="-d"):
+        print("製作中...")
     else:
-        global x
-        global Prompt
-        global Weights
         request= request.strip(',');
         request = re.sub('[{}()\n]','',request)
         Promptlist=re.split(r'[,:]',request)
@@ -51,6 +60,7 @@ def process(request):
             for i in range(len(Prompt)):
                 print("("+Prompt[i]+":"+Weights[i]+"),",end = '')
             x+=1
+        print("\n")
         
 
 
